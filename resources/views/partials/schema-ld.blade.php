7b443b9732bd8ab5c8ccccb03112d8e2abc8e2fa
Eitan <eitansheli2019@gmail.com>
2025-12-23 14:45:27 +0200

    Add comprehensive training types dropdown with categories

--- resources/views/partials/schema-ld.blade.php
+++ resources/views/partials/schema-ld.blade.php
@@ -5,36 +5,37 @@
     $logoUrl = file_exists(public_path('logo.png')) 
         ? $baseUrl . '/logo.png' 
         : ($baseUrl . '/favicon.ico');
+    
+    // Build JSON-LD structure
+    $schemaData = [
+        '@context' => 'https://schema.org',
+        '@graph' => [
+            [
+                '@type' => 'Organization',
+                'name' => $appName,
+                'url' => $baseUrl,
+                'logo' => $logoUrl,
+                'sameAs' => [
+                    'https://www.instagram.com/fitmatch.co.il/',
+                    'https://www.tiktok.com/@fitmatch912'
+                ]
+            ],
+            [
+                '@type' => 'WebSite',
+                'name' => $appName,
+                'url' => $baseUrl,
+                'potentialAction' => [
+                    '@type' => 'SearchAction',
+                    'target' => [
+                        '@type' => 'EntryPoint',
+                        'urlTemplate' => $baseUrl . '/trainers?search={search_term_string}'
+                    ],
+                    'query-input' => 'required name=search_term_string'
+                ]
+            ]
+        ]
+    ];
 @endphp
 
-<script type="application/ld+json">
-{
-    "@context": "https://schema.org",
-    "@graph": [
-        {
-            "@type": "Organization",
-            "name": "{{ $appName }}",
-            "url": "{{ $baseUrl }}",
-            "logo": "{{ $logoUrl }}",
-            "sameAs": [
-                "https://www.instagram.com/fitmatch.co.il/",
-                "https://www.tiktok.com/@fitmatch912"
-            ]
-        },
-        {
-            "@type": "WebSite",
-            "name": "{{ $appName }}",
-            "url": "{{ $baseUrl }}",
-            "potentialAction": {
-                "@type": "SearchAction",
-                "target": {
-                    "@type": "EntryPoint",
-                    "urlTemplate": "{{ $baseUrl }}/trainers?search={search_term_string}"
-                },
-                "query-input": "required name=search_term_string"
-            }
-        }
-    ]
-}
-</script>
+<script type="application/ld+json">{!! json_encode($schemaData, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT) !!}</script>
 
